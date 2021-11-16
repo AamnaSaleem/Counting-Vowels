59fbf62486dc2b53d222510daa80b97bd49708cf
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-16 21:05:51 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Str1, VowStr As String
+        Dim ThisChar As Char
+        Dim VowTimes As Integer
+
+        VowStr = "AEIOUaeiou"
+        VowTimes = 0
+
+        Console.Write("Enter string to be processed: ")
+        Str1 = Console.ReadLine
+
+        For a = 1 To Len(Str1)
+            ThisChar = Mid(Str1, a, 1)
+            If InStr(VowStr, ThisChar) Then
+                VowTimes = VowTimes + 1
+
+            End If
+        Next
+
+        Console.WriteLine(" Number of vowels in entered string are: " & VowTimes)
+        Console.ReadKey()
+    End Sub
+
+End Module
